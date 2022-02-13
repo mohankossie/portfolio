b80209ade21a2ed81d7a09ec01bc9361f6d75437
mohankossie <mohankossie@gmail.com>
2022-02-13 08:09:03 +0200

    Deployed

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+//src/components/About.js
+import React from "react";
+
+export default function() {
+    return(
+        <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                        Hi, I'm Moha.
+                        <br className="hidden lg:inline-block" /> I love to build amazing apps. 
+                    </h1>
+                    <p className="mb-8 leading-relaxed text-white">
+                    Moha Nkossie is currently a Freelance Web Designer and Developer with over 5 years of experience, specializing in designing all-time responsive wordpress websites and building rich and collaborative applications on Google’s firebase. He has gathered a lot of experience and skills as he has had the honor of working with clients from all the 4 corners of the world.
+                    </p>
+                    <div className="flex justify-center">
+                        <a href="#contact" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                            Work With Me
+                        </a>
+                        <a href="#projects" className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg ">
+                            See My Past Work
+                        </a>
+                    </div>
+                </div>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                    <img className="object-cover object-center rounded" alt="hero" src="./coding.svg" />
+                </div>
+            </div>
+        </section>
+    )
+}
